fa5d7da7686043b194c728fb560538ef933ab4e4
nast90210 <muxaul88@gmail.com>
2025-12-08 12:40:39 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ DISK_IMAGE_NAME=arch-linux-arm-sp11.img
 DISK_IMAGE_SIZE_MB=10000
 
 KERNEL_GIT_REPO=https://github.com/dwhinham/kernel-surface-pro-11
-KERNEL_GIT_BRANCH=x1e80100-rc7-6.17-sp11
+KERNEL_GIT_BRANCH=x1e80100-6.18-rc7-sp11
 
 # If "rc" in the branch name then take the config from the ALARM -rc package
 if [[ $KERNEL_GIT_BRANCH == *rc* ]]; then
